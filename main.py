44a25be01f2371be5d7b1d28582a47e916c26009
Marshal10 <marshalmaleane123@gmail.com>
2024-02-10 21:13:03 +0530

    Append and string representation

--- main.py
+++ main.py
@@ -10,5 +10,28 @@ class DoublyLinkedList:
         self.tail=None
         self.length=0
         
+    def __str__(self):
+        temp=self.head
+        result=''
+        while temp:
+            result+=str(temp.value)
+            if temp.next:
+                result+=" <-> "
+            temp=temp.next
+        return result
+        
+    def append(self,value):
+        new_node=Node(value)
+        if self.head is None:
+            self.head=new_node
+            self.tail=new_node
+        else:
+            self.tail.next=new_node
+            new_node.prev=self.tail
+            self.tail=new_node
+        self.length+=1
+        
 doubly_linked_list=DoublyLinkedList()
+doubly_linked_list.append(10)
+doubly_linked_list.append(20)
 print(doubly_linked_list)
