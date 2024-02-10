ad4f0c44a55daf1e670fd81c3e62dd8b52a43962
Marshal10 <marshalmaleane123@gmail.com>
2024-02-10 21:16:01 +0530

    Prepend

--- main.py
+++ main.py
@@ -31,7 +31,20 @@ class DoublyLinkedList:
             self.tail=new_node
         self.length+=1
         
+    def prepend(self,value):
+        new_node=Node(value)
+        if self.head is None:
+            self.head=new_node
+            self.tail=new_node
+        else:
+            self.head.prev=new_node
+            new_node.next=self.head
+            self.head=new_node
+        self.length+=1
+        
 doubly_linked_list=DoublyLinkedList()
 doubly_linked_list.append(10)
 doubly_linked_list.append(20)
+print(doubly_linked_list)
+doubly_linked_list.prepend(5)
 print(doubly_linked_list)
